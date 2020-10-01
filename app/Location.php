1202bd10ee2db9337eea960d643b1686dffa9568
Michele Curtaz <mcurtaz@gmail.com>
2020-10-01 17:56:09 +0200

    correzione function model

--- app/Location.php
+++ app/Location.php
@@ -13,6 +13,6 @@ class Location extends Model
 
     public function employees()
     {
-        return $this->belongsToMany(Employee::class);
+        return $this->hasMany(Employee::class);
     }
 }
