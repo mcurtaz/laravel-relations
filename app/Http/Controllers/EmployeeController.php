0ff8fa5d14cf1cfa1375736f4aeec1bf45b868e3
Michele Curtaz <mcurtaz@gmail.com>
2020-10-02 17:05:30 +0200

    show - create

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Employee;
+use App\Location;
 
 class EmployeeController extends Controller
 {
@@ -12,5 +13,28 @@ class EmployeeController extends Controller
         $employees = Employee::all();
 
         return view('employees.index', compact('employees'));
+    }
+
+    public function show($id){
+        
+        $employee = Employee::findOrFail($id);
+
+        return view('employees.show', compact('employee'));
+    }
+
+    public function create(){
+
+        $locations = Location::all();
+
+        return view('employees.create', compact('locations'));
+    }
+
+    public function store(Request $request){
+       
+        $data = $request -> all();
+
+        Employee::create($data);
+
+        return redirect() -> route('employee-index');
     }
 }
